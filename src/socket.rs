d72a3036d4a2f2b3c0e6c6f884435f93219ab8b9
Markus Reiter <me@reitermark.us>
2018-12-16 22:20:54 +0100

    Modularize functions.

---
+++ src/socket.rs
@@ -0,0 +1,121 @@
+use super::*;
+
+const LISTEN_QUEUE: c_int = 128;
+
+#[no_mangle]
+pub unsafe extern fn openSocket(tcpport: c_int) -> c_int {
+  let mut hints: addrinfo = mem::zeroed();
+
+  match inetversion {
+    6 => { hints.ai_family = PF_INET6 as _; },
+    _ => { hints.ai_family = PF_INET as _; },
+  }
+
+  hints.ai_socktype = SOCK_STREAM as _;
+  hints.ai_protocol = IPPROTO_TCP as _;
+  hints.ai_flags    = AI_PASSIVE;
+
+  let port = CString::new(tcpport.to_string()).unwrap();
+
+  let mut res: *mut addrinfo = ptr::null_mut();
+
+  let n = getaddrinfo(ptr::null(), port.into_raw(), &hints as *const addrinfo, &mut res as *mut *mut addrinfo);
+
+  if n < 0 {
+    let error_message = CStr::from_ptr(gai_strerror(n)).to_str().unwrap();
+    log_it!(LOG_ERR, "getaddrinfo error: [{}]\n", error_message);
+    return -1
+  }
+
+  let ressave = res.clone();
+
+  let mut listenfd = -1;
+
+  while let Some(curr_res) = res.as_ref() {
+    listenfd = socket(curr_res.ai_family, curr_res.ai_socktype, curr_res.ai_protocol);
+
+    let optval: *const c_int = &1;
+
+    if listenfd >= 0 {
+      if setsockopt(listenfd, SOL_SOCKET as _, SO_REUSEADDR as _, optval as *const c_void, mem::size_of_val(&*optval) as u32) < 0 {
+        log_it!(LOG_ERR, "setsockopt failed!");
+        exit(1);
+      }
+
+      if bind(listenfd, curr_res.ai_addr as *mut _, curr_res.ai_addrlen) == 0 {
+        break
+      }
+
+      close(listenfd);
+      listenfd = -1;
+    }
+
+    res = curr_res.ai_next;
+  }
+
+  freeaddrinfo(ressave);
+
+  if listenfd < 0 {
+    eprintln!("socket error: could not open socket");
+    exit(1);
+  }
+
+  listen(listenfd, LISTEN_QUEUE);
+
+  log_it!(LOG_NOTICE, "TCP socket {} opened", tcpport);
+
+  listenfd
+}
+
+#[no_mangle]
+pub unsafe extern fn listenToSocket(listenfd: c_int, makeChild: c_int, checkP: extern fn(ip: *mut c_char) -> c_short) -> c_int {
+  let _ = checkP;
+
+  let mut cliaddr: sockaddr_storage = mem::zeroed();
+
+  signal(SIGCHLD as _, SIG_IGN);
+
+  let cliaddrlen: *const usize = &mem::size_of_val(&cliaddr);
+
+  loop {
+    let connfd = accept(listenfd, &mut cliaddr as *mut sockaddr_storage as *mut sockaddr, cliaddrlen as *mut u32);
+
+    let mut client_host: [c_char; NI_MAXHOST as usize] = mem::zeroed();
+    let mut client_service: [c_char; NI_MAXSERV as usize] = mem::zeroed();
+
+    getnameinfo(&mut cliaddr as *mut _ as *mut _, *cliaddrlen as _, client_host.as_mut_ptr(), NI_MAXHOST, client_service.as_mut_ptr(), NI_MAXSERV, NI_NUMERICHOST);
+
+    let client_host = CStr::from_ptr(client_host.as_ptr());
+    let client_service = CStr::from_ptr(client_service.as_ptr());
+
+    if connfd < 0 {
+        log_it!(LOG_NOTICE, "accept on host {}: port {}", client_host.to_str().unwrap(), client_service.to_str().unwrap());
+        close(connfd);
+        continue
+    }
+
+    log_it!(LOG_NOTICE, "Client connected {}:{} (FD:{})", client_host.to_str().unwrap(), client_service.to_str().unwrap(), connfd);
+
+    if makeChild == 0 {
+      return connfd;
+    } else {
+      match fork() {
+        0 => {
+          close(listenfd);
+          return connfd;
+        },
+        childpid => {
+          log_it!(LOG_INFO, "Child process started with pid {}", childpid);
+        }
+      }
+    }
+
+    close(connfd);
+  }
+}
+
+#[no_mangle]
+pub unsafe extern fn closeSocket(sockfd: c_int) {
+  log_it!(LOG_INFO, "Closed connection (fd:{})", sockfd);
+  close(sockfd);
+}

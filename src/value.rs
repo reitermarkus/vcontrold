8cba000f19b0f8524ab754aee79b66a9de1bdc14
Markus Reiter <me@reitermark.us>
2019-01-03 11:46:07 +0100

    Add `FromStr` for `Value`.

--- src/value.rs
+++ src/value.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use serde_derive::*;
 
 use crate::types::{SysTime, CycleTime};
@@ -9,4 +11,27 @@ pub enum Value {
   SysTime(SysTime),
   CycleTime(CycleTime),
   String(String),
+}
+
+#[derive(Debug)]
+pub enum Never {}
+
+impl FromStr for Value {
+  type Err = Never;
+
+  fn from_str(s: &str) -> Result<Value, Self::Err> {
+    if let Ok(number) = s.parse::<f64>() {
+      return Ok(Value::Number(number))
+    }
+
+    if let Ok(systime) = s.parse::<SysTime>() {
+      return Ok(Value::SysTime(systime))
+    }
+
+    if let Ok(cycletime) = s.parse::<CycleTime>() {
+      return Ok(Value::CycleTime(cycletime))
+    }
+
+    Ok(Value::String(s.to_owned()))
+  }
 }
